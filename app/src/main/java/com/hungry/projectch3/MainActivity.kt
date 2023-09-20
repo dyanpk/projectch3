02238a6fe73b983fa960967665bf1ea2703f13f3
Dian Permata Kusuma <diankusuma664@gmail.com>
2023-09-20 15:17:24 +0700

    Initial commit

---
+++ app/src/main/java/com/hungry/projectch3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hungry.projectch3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
